b3dd608d3ffeddd8f0e309d3758f17fda64572c7
Lucas Sousa <70826073+leitecsleite@users.noreply.github.com>
2023-02-07 11:42:57 -0300

    Feat(Class-01):Adding the amplification header

--- src/server.js
+++ src/server.js
@@ -1,6 +1,26 @@
 import http from 'http'
 
+const users = []
+
 const server = http.createServer((req, res) =>{
+
+ const {method, url} = req
+
+  if (method === 'GET' && url === '/users'){
+    return res
+    .setHeader("Content-type", 'application/json')
+    .end(JSON.stringify(users))
+  }
+
+  if(method === "POST" && url === "/users"){
+    users.push({
+        id:1,
+        name: 'Lucas Leite', 
+        email: 'lucas@alguma.com.br'
+    })
+    return res.end("criação de usuário")
+  }
+ 
   return res.end('Hello World')
 })
 
