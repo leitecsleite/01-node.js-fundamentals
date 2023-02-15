db3de7dd223288defaaa46d75afe05540fda3c20
Lucas Sousa <70826073+leitecsleite@users.noreply.github.com>
2023-02-15 15:14:38 -0300

    Feat(master):Capturing query parameters.

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import http from 'http'
 import { json } from './middlewares/json.js'
 import { routes } from './routes.js'
+import { extractQueryParams } from './utils/extract-query-params.js'
 
 
 
@@ -8,8 +9,6 @@ import { routes } from './routes.js'
 const server = http.createServer(async (req, res) => {
 
    const { method, url } = req
-
-   console.log(method, url)
 
    await json(req, res)
 
@@ -20,7 +19,10 @@ const server = http.createServer(async (req, res) => {
     if(route){
         const routeParams = req.url.match(route.path)
 
-        req.params = {...routeParams.groups} 
+        const { query, ...params} = routeParams.groups
+
+        req.params = params
+        req.query = query ? extractQueryParams(query) : {}
 
         return route.handle(req, res)
     }
